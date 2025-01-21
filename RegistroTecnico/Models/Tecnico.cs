6080ac353090e9b42ce934313c7530e926908f96
Adrian Hernández Bonilla <adrian_hernandez@ucne.edu.do>
2025-01-21 09:48:34 -0400

    Creacion de la carpeta Models junto con la clase Tecnico para la migracion

---
+++ RegistroTecnico/Models/Tecnico.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnico.Models
+{
+    namespace RegistroTecnicos.Models
+    {
+        public class Tecnicos
+        {
+            [Key]
+            public int TecnicoID { get; set; }
+
+
+            [Required(ErrorMessage = "este campo es obligatorio")]
+
+            public string Nombre { get; set; } = string.Empty!;// para que nombre no sea nulo
+            public int SueldoHora { get; set; }
+
+        }
+    }
+
+}
