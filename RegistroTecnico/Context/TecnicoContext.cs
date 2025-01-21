ed93411bfd9d4a483f49372b9349c025f0845359
Adrian Hernández Bonilla <adrian_hernandez@ucne.edu.do>
2025-01-21 10:10:48 -0400

    Debido a un error de escritura se rescribe los nombres en Name space de manera correcta para evitar fallos

--- RegistroTecnico/Context/TecnicoContext.cs
+++ RegistroTecnico/Context/TecnicoContext.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using RegistroTecnico.Models.RegistroTecnicos.Models;
 
 
-namespace RegistroTecnicos.Context
+namespace RegistroTecnico.Context
 {
     public class TecnicoContext : DbContext
     {
