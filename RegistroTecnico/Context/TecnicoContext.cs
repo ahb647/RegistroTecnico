8c3668a9c8accf7e296d844eddd1b120f78ccfea
Adrian Hernández Bonilla <adrian_hernandez@ucne.edu.do>
2025-01-21 09:50:19 -0400

    Creacion de la carpeta Context la cual servira para la creacion de la migracion

---
+++ RegistroTecnico/Context/TecnicoContext.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using RegistroTecnico.Models.RegistroTecnicos.Models;
+
+
+namespace RegistroTecnicos.Context
+{
+    public class TecnicoContext : DbContext
+    {
+
+        public TecnicoContext(DbContextOptions<TecnicoContext> options) : base(options) { }
+
+        public DbSet<Tecnicos> Tecnicos { get; set; }
+    }
+
+
+}
