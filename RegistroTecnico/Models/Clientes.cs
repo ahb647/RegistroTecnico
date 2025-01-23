f7802c6a720217b4e948e27bc94866b778606f9e
Adrian Hernández Bonilla <adrian_hernandez@ucne.edu.do>
2025-01-23 17:28:51 -0400

    Se agregaron los datos que contendran los clientes

---
+++ RegistroTecnico/Models/Clientes.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnico.Models
+{
+    public class Clientes 
+    {
+
+        [Key]
+        public int ClienteID { get; set; }
+        public int TecnicoID { get; set; }
+
+        public string? Nombres {  get; set; }    
+
+        public string? Direccion {  get; set; }
+        public int Rnc {  get; set; }
+
+        public Decimal LimiteCredito { get; set; }
+        public DateTime FechaIngreso { get; set; }  
+
+
+
+    }
+}
