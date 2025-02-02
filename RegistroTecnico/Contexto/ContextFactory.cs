fd96497ad8dbf1fdc50e385a86be3795e7dd1720
Adrian Hernández Bonilla <adrian_hernandez@ucne.edu.do>
2025-02-02 18:28:44 -0400

    EL nombre de la carpeta fue cambiado al que corresponde

---
+++ RegistroTecnico/Contexto/ContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace RegistroTecnico.Context
+{
+    public class ContextoFactory : IDesignTimeDbContextFactory<TecnicoContext>
+    {
+        public TecnicoContext CreateDbContext(string[] args)
+        {
+            // Configura el DbContext usando la cadena de conexión de Somee.
+            var optionsBuilder = new DbContextOptionsBuilder<TecnicoContext>();
+            optionsBuilder.UseSqlServer("workstation id=AdrianDB.mssql.somee.com;packet size=4096;user id=AHB_SQLLogin_1;pwd=419y65tsvd;data source=AdrianDB.mssql.somee.com;persist security info=False;initial catalog=AdrianDB;TrustServerCertificate=True");
+
+            return new TecnicoContext(optionsBuilder.Options);
+        }
+    }
+}
